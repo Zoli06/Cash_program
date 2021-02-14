dfa930ef41ecf67255ba6e7509ff14a48193e2f6
Zoli06 <63035911+Zoli06@users.noreply.github.com>
2021-02-14 19:09:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var mysql = require("mysql");
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Zoli94631782",
+  password: "*****",
 });
 
 function getMIME(url) {
